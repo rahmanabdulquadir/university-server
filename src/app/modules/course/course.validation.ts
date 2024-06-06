4e9724c56e737a12fb7c99abb688a1c4bf559d78
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-06-06 21:04:51 +0600

    course module added

---
+++ src/app/modules/course/course.validation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const preRequisiteCourseValidationSchema = z.object({
+  body: z.object({
+    course: z.string(),
+    isDeleted: z.boolean(),
+  }),
+});
+
+const createCourseValidationSchema = z.object({
+  body: z.object({
+    title: z.string(),
+    prefix: z.string(),
+    code: z.number(),
+    credits: z.number(),
+    isDeleted: z.boolean(),
+    preRequisiteCourses: z.array(preRequisiteCourseValidationSchema),
+  }),
+});
+
+export const CourseValidations = {
+  createCourseValidationSchema,
+}
